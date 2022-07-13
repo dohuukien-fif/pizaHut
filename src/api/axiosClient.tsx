b1a77903cd8acba5b24112b76914dfaa793b9c42
huu kien <huukien0099@gmail.com>
2022-07-13 22:39:08 +0700

    api

--- src/api/axiosClient.tsx
+++ src/api/axiosClient.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import queryString from 'query-string';
 const axiosClient = axios.create({
   // https://api-pizza-home.herokuapp.com/
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://api-pizza-home.herokuapp.com/api',
   headers: {
     'Content-type': 'application/json',
   },
